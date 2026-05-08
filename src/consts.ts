793560c82fe41324fa91b8d1de8db6b001632360
laiziki486 <118620344+laiziki486@users.noreply.github.com>
2026-05-08 16:34:56 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '时间的碎片';
-export const SITE_DESCRIPTION = '漂浮在记忆与遗忘之间';
+export const SITE_DESCRIPTION = '漂浮在记忆与遗忘中';
